1d7f2b34a00572ae656e3cc30d0ab2b723c8e401
Matt Weiss <weiss.matt@outlook.com>
2019-06-29 12:33:03 -0600

    Spikes experimentation with request using Astronomy picture of the day

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,6 +1,20 @@
 var express = require("express");
 var router = express.Router();
 var User = require('../../../models').User;
+var request = require('request');
 
+var api_call = request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
+    if (err) {
+      return err;
+    }else{
+      return [res.statusCode,body]
+    }
+    })
+
+
+router.get("/", function(req, res, next){
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).send(JSON.stringify(api_call["responseContent"]["body"], null, ' '));
+});
 
 module.exports = router;
